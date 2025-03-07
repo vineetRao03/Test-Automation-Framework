e6274b792c9706be8c1ad0fb5579d1b515a2f44b
Vineet <vineetrao0309@gmail.com>
2025-03-07 19:24:42 +0530

    made corrections in the path

--- src/test/java/com/utility/JSONUtility.java
+++ src/test/java/com/utility/JSONUtility.java
@@ -14,7 +14,7 @@ public class JSONUtility {
 	public static Environment readJSON (Env env) {
 
 		Gson gson = new Gson(); // to read JSON in java- No in-built funct. so we use 
-		File jsonFile = new File(System.getProperty("user.dir") + "\\config\\config.json");
+		File jsonFile = new File(System.getProperty("user.dir") + "//config//config.json");
 		FileReader fileReader = null;
 		try {
 			fileReader = new FileReader(jsonFile);
--- src/test/java/com/utility/PropertiesUtil.java
+++ src/test/java/com/utility/PropertiesUtil.java
@@ -13,7 +13,7 @@ public class PropertiesUtil {
 
 	public static String readProperty(Env env, String propertyName) {
 		System.out.println(System.getProperty("user.dir"));
-		File propFile = new File(System.getProperty("user.dir") + "\\config\\" + env + ".properties");
+		File propFile = new File(System.getProperty("user.dir") + "//config//" + env + ".properties");
 		FileReader fileReader = null;
 		Properties properties = new Properties();
 		try {
